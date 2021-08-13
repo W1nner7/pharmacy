2ba0f78b88c3b24ceac0199ad672797fef9448f9
Yevhenii Rizchenko <veuzqpvo@email.com>
2021-08-13 06:22:06 +0300

    update configs

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  css: {
+    requireModuleExtension: false,
+  },
+  // scss: {
+  //   additionalData: `@import "~@/assets/scss/*.scss";`,
+  // },
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === "production" ? "/pharmacy/" : "/",
+};
